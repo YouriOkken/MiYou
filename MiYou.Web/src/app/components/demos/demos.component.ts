35d8592cb2831ce48f6a6de212e7c33bf316b41d
Milana Deriga <milderiga@gmail.com>
2025-07-09 22:34:57 +0200

    finished animation for scrolling with buttons

--- MiYou.Web/src/app/components/demos/demos.component.ts
+++ MiYou.Web/src/app/components/demos/demos.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Demo1Component } from "./demo1/demo1.component";
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
     selector: 'demos-component',
     templateUrl: 'demos.component.html',
     styleUrl: 'demos.component.scss',
-    imports: [Demo1Component]
+    imports: [Demo1Component, LottieComponent]
 })
 
 export class DemosComponent implements OnInit {
+  @ViewChild('previewRow', { static: false }) previewRow!: ElementRef;
+
     constructor() { }
 
     ngOnInit() { }
+
+    scrollAnimation: AnimationOptions = {
+        path: 'assets/animations/Animation-1752090248146.json',
+        loop: true,
+        autoplay: true,
+    };
+
+    scrollRight() {
+        const container = this.previewRow.nativeElement;
+        const scrollAmount = container.offsetWidth * 1;
+        container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+
+    scrollLeft() {
+        const container = this.previewRow.nativeElement;
+        const scrollAmount = container.offsetWidth * 1;
+        container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    }
 }
