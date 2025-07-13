7d7e189ba2647fe53d345378c52991ca3d9bfb99
Milana Deriga <milderiga@gmail.com>
2025-07-13 14:23:18 +0200

    bezig met pricing

--- MiYou.Web/src/app/app.routes.ts
+++ MiYou.Web/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'pricing', component: PricingComponent}
 ];
---
+++ MiYou.Web/src/app/components/pricing/pricing.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pricing-component',
+    templateUrl: 'pricing.component.html',
+    styleUrl: 'pricing.component.scss'
+})
+
+export class PricingComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
