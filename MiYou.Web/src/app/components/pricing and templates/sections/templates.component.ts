32074a7436b035c2d01d2434977a1d4f5051442e
Milana Deriga <milderiga@gmail.com>
2025-07-14 23:06:20 +0200

    begonnen met templates en spacing van pricing gefixed

---
+++ MiYou.Web/src/app/components/pricing and templates/sections/templates.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'templates-component',
+    templateUrl: 'templates.component.html',
+    styleUrl: 'templates.component.scss'
+})
+
+export class TemplatesComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
