4f4f635ee740fdc4ad913c8dfd611644f01e3447
Milana Deriga <milderiga@gmail.com>
2025-07-18 14:15:09 +0200

    begonna

--- MiYou.Web/src/app/components/pricing and templates/pricing-and-templates.component.ts
+++ MiYou.Web/src/app/components/pricing and templates/pricing-and-templates.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TemplatesComponent } from "./sections/templates.component";
 
 @Component({
     selector: 'pricing-and-templates-component',
     templateUrl: 'pricing-and-templates.component.html',
-    styleUrl: 'pricing-and-templates.component.scss'
+    styleUrl: 'pricing-and-templates.component.scss',
+    imports: [TemplatesComponent]
 })
 
 export class PricingAndTemplatesComponent implements OnInit {
