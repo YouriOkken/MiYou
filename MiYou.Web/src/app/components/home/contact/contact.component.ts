514c06f2af2ca0775a14047fd74d84223ee20ce7
M.Deriga <milderiga@gmail.com>
2025-07-29 23:52:34 +0200

    fixed youri's error

--- MiYou.Web/src/app/components/home/contact/contact.component.ts
+++ MiYou.Web/src/app/components/home/contact/contact.component.ts
@@ -17,7 +17,7 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ContactComponent implements OnInit {
   contactForm!: FormGroup;
 
-  isSuccess: boolean | undefined = true;
+  isSuccess: boolean | undefined;
   isProcessing: boolean = false;
   errorMessage: string = '';
 
