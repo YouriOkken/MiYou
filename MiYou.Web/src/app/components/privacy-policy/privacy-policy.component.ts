bbb9701f7946a958e625a1b710f31f1c7d7e274c
M.Deriga <milderiga@gmail.com>
2025-07-29 23:06:32 +0200

    fixed privacy policy

--- MiYou.Web/src/app/components/privacy-policy/privacy-policy.component.ts
+++ MiYou.Web/src/app/components/privacy-policy/privacy-policy.component.ts
@@ -3,12 +3,13 @@ import { RouterModule } from '@angular/router';
 import { getAnimation } from '../../utilities/animations/animation.utilities';
 import { animationTypes } from '../../utilities/enums/animationTypes.enum';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'privacy-policy-component',
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss'],
-  imports: [RouterModule, LottieComponent]
+  imports: [RouterModule, LottieComponent, TranslateModule]
 })
 export class PrivacyPolicyComponent {
   privacyPolicyAnimation: AnimationOptions;
