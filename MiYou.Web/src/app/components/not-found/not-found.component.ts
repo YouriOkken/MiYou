75b0f0ea718ee8af43b6a2170ce2e051baa7a349
Just_Gamer <114059854+Just-GamerNL@users.noreply.github.com>
2025-07-29 23:23:56 +0200

    fixed translations

--- MiYou.Web/src/app/components/not-found/not-found.component.ts
+++ MiYou.Web/src/app/components/not-found/not-found.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalComponent } from "../../modal/modal.component";
 import { Router } from '@angular/router';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 import { getAnimation } from '../../utilities/animations/animation.utilities';
 import { animationTypes } from '../../utilities/enums/animationTypes.enum';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'not-found-component',
     templateUrl: 'not-found.component.html',
     styleUrl: 'not-found.component.scss',
-    imports: [LottieComponent]
+    imports: [LottieComponent, TranslateModule]
 })
 
 export class NotFoundComponent implements OnInit {
