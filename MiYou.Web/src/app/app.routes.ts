bd039836a186cc884e1addd11de32386783678e0
Milana Deriga <milderiga@gmail.com>
2025-08-06 19:40:51 +0200

    bezig met TOS allene nog inhoud veranderen en de lottie fiels

--- MiYou.Web/src/app/app.routes.ts
+++ MiYou.Web/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PricingAndTemplatesComponent } from './components/pricing and templates
 import { CookiesComponent } from './components/cookies/cookies.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { TermsOfServiceComponent } from './components/terms-of-service/terms-of-service.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'pricing', component: PricingAndTemplatesComponent},
     {path: 'cookies', component: CookiesComponent},
     {path: 'privacy-policy', component: PrivacyPolicyComponent},
+    {path: 'terms-of-service', component: TermsOfServiceComponent},
     {path: '**', component: NotFoundComponent} // MOET ALS LAATSTE
 ];
