dfe22d0b0c1cb9b450498de5d8ab655f701a1b85
Milana Deriga <milderiga@gmail.com>
2025-08-13 12:09:41 +0200

    language switch anders gemaakt naar n arrow en ernaast word laten zien welke taal je gekozen hebt

--- MiYou.Web/src/app/components/navbar/navbar.component.ts
+++ MiYou.Web/src/app/components/navbar/navbar.component.ts
@@ -1,51 +1,47 @@
-import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'nav-component',
     templateUrl: 'navbar.component.html',
-    styleUrl: "navbar.component.scss",
+    styleUrls: ['navbar.component.scss'],
     imports: [RouterModule, TranslateModule]
 })
-
 export class NavbarComponent {
     menuOpen = false;
     languageDropdownOpen = false;
+    currentLanguage: string;
 
-    constructor(private eRef: ElementRef, private translate: TranslateService) { }
+    constructor(private eRef: ElementRef, private translate: TranslateService) {
+        this.currentLanguage = this.translate.currentLang || 'en';
+    }
 
     toggleMenu() {
         this.menuOpen = !this.menuOpen;
     }
 
-    //kijk of er clicks buiten de menu's zijn geweest om ze te sluiten. geldt voor hamburger en translate menu
     @HostListener('document:click', ['$event'])
     handleClickOutside(event: MouseEvent) {
         const target = event.target as HTMLElement;
 
-        if (
-            this.menuOpen &&
-            !this.eRef.nativeElement.contains(target)
-        ) {
+        if (this.menuOpen && !this.eRef.nativeElement.contains(target)) {
             this.menuOpen = false;
         }
 
-        if (
-            this.languageDropdownOpen &&
-            !this.eRef.nativeElement.contains(target)
-        ) {
+        if (this.languageDropdownOpen && !this.eRef.nativeElement.contains(target)) {
             this.languageDropdownOpen = false;
         }
     }
 
     toggleLanguageDropdown(event: Event) {
-        event.stopPropagation(); //voorkomt dat de dropdown zich instant sluit
+        event.stopPropagation();
         this.languageDropdownOpen = !this.languageDropdownOpen;
     }
 
     setLanguage(lang: string) {
         this.translate.use(lang);
+        this.currentLanguage = lang;
         if (typeof window !== 'undefined' && window.localStorage) {
             localStorage.setItem('selectedLang', lang);
         }
